36b714c3ed1292dd4a171b8f87c787a658b774a6
Giuliane Oliveira <pgsdoliveira@inf.ufpel.edu.br>
2020-08-14 00:11:33 -0300

    Atualizando diretório css-html

--- CSS-HTML-JS/HTML/exercicios/forms/server.js
+++ CSS-HTML-JS/HTML/exercicios/forms/server.js
@@ -6,7 +6,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (requisicao, resposta)=>{
     console.log(requisicao.body) //Mostra todas as infos do form
-    resposta.send('Tudo certo!')
+    resposta.send('<h1>Usuário incluído.</h1>')
+})
+app.post('/usuarios/:id', (requisicao, resposta)=>{
+    console.log(requisicao.params.id)
+    console.log(requisicao.body)
+    resposta.send('<h1>Usuário Alterado!</h1>')
 })
 
 app.listen(3003)
